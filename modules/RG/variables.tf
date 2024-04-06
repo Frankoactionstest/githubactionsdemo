3a83facd89a996b0722c9575f263f73bfeee6fbf
Franklin <addofrancium@gmail.com>
2024-04-06 11:27:31 -0400

    first commit

---
+++ modules/RG/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+    type = string
+}
+
+variable "environment" {
+  type    = string
+}
+
+variable "owner" {
+  type    = string
+}
+
+variable "rg_name" {
+  type    = string
+}
+
+variable "plan_name" {
+  type    = string
+}
+
+variable "vnet_name" {
+  type    = string
+}
+
+variable "subnet_name" {
+  type    = string
+}
