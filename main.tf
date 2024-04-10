00fee0a921024ee80de1b582740490f7712d444f
Franklin <addofrancium@gmail.com>
2024-04-10 14:10:31 -0400

    adding multi envs

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-# module "RG" {
-#   source = "./modules/RG"
+module "RG" {
+  source = "./modules/RG"
 
-#   owner = var.owner
-#   environment = var.environment
-#   region = var.region
-#   rg_name = var.rg_name
-# }
+  owner = var.owner
+  environment = var.environment
+  region = var.region
+  rg_name = var.rg_name
+}
--- modules/RG/main.tf
+++ modules/RG/main.tf
@@ -1,16 +1,16 @@
-resource "azurerm_resource_group" "actionstest" {
-  name   = var.rg_name
-  location = var.region
-  tags = {
-    environment = var.environment
-    owner       = var.owner
-  }
-}
+# resource "azurerm_resource_group" "test" {
+#   name   = var.rg_name
+#   location = var.region
+#   tags = {
+#     environment = var.environment
+#     owner       = var.owner
+#   }
+# }
 
 resource "azurerm_storage_account" "example" {
   name                     = "test2torromenia"
-  resource_group_name      = azurerm_resource_group.actionstest.name
-  location                 = azurerm_resource_group.actionstest.location
+  resource_group_name      = var.rg_name
+  location                 = var.region
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
