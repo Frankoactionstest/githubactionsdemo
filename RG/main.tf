ecb17dd11491600b15f8cdc951919fb641f01077
Franklin <addofrancium@gmail.com>
2024-04-10 19:58:17 -0400

    removed root folder files

--- RG/main.tf
+++ RG/main.tf
@@ -6,6 +6,25 @@
 #     owner       = var.owner
 #   }
 # }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+
+  backend "azurerm" {
+    key                  = "test3.terraform.tfstate"
+  }
+  required_version = ">=0.12"
+}
+
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
 
 resource "azurerm_storage_account" "example" {
   name                     = "test2torromenia"
